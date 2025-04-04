24ad877dce944556ad27876ab1bc6450e16e225e
Nick van der Burgt <nvanderburgt@infiniot.nl>
2025-04-04 15:05:54 +0200

    .

--- testdeployofpypi/exampleclass.py
+++ testdeployofpypi/exampleclass.py
@@ -6,10 +6,6 @@ from typing import Optional
 class Example:
     """Example class."""
 
-    def __init__(
-            self,
-            name: str,
-            count: Optional[int] = None
-    ) -> None:
+    def __init__(self, name: str, count: Optional[int] = None) -> None:
         self.name = name
         self.count = count
--- testdeployofpypi/otherexample.py
+++ testdeployofpypi/otherexample.py
@@ -2,12 +2,10 @@ from abc import abstractmethod
 from typing import Optional
 from src.exampleclass import Example
 
+
 class NewExample(Example):
     def __init__(
-            self,
-            name: str,
-            description: str,
-            count: Optional[int] = None
+        self, name: str, description: str, count: Optional[int] = None
     ) -> None:
         self.description = description
         super().__init__(name, count)
