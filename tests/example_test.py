b3de6985e2074b126f61cf72a2b1332cbcb72b0d
Nick van der Burgt <nvanderburgt@infiniot.nl>
2025-04-07 10:24:21 +0200

    Added test, restructured

---
+++ tests/example_test.py
@@ -0,0 +1,6 @@
+"""Module which contains an example test to validate test workflow."""
+
+
+def test_example() -> None:
+    """Example test that asserts true to validate pipeline and test coverage working."""
+    assert 1
